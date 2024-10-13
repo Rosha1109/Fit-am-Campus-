434c4ce8e53646c39b9e61f94071b27d000b1d71
Roshanan <roshanan.muthuvelu@smail.th-koeln.de>
2024-10-13 21:43:33 +0200

    Initial commit

---
+++ src/main/java/de/thk/gm/gdw/fitamcampus/FitamcampusApplication.java
@@ -0,0 +1,13 @@
+package de.thk.gm.gdw.fitamcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitamcampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitamcampusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/thk/gm/gdw/fitamcampus/FitamcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thk.gm.gdw.fitamcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitamcampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
